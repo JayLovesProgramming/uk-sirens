377278caefeb15296426032a09fe716641145b72
Jay <jaydenxwragg@outlook.com>
2023-03-06 23:47:06 +0000

    Updated Fxmanifest to make it easier to understand

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -5,22 +5,6 @@ author 'Made by Jay (SlumCity#0001)'
 lua54 'yes'
 
 client_scripts {
-    "config.lua",
-    "client/init.lua",
-    "client/keyPress.lua",
-    "client/updateLights.lua",
-    "client/lights/enviromentLights.lua",
-    "client/lights/primaryLights.lua",
-    "client/lights/secondaryLights.lua",
-    "client/lights/warningLights.lua",
-    "client/lights/rearPrimaryLights.lua",
-    "client/lights/frontPrimaryLights.lua",
-    "client/lights/wigWagLights.lua",
-    "client/utils/utils.lua",
-    "client/updateSiren.lua",
-    "client/toggleBullHorn.lua",
-    "client/lights/spotlight.lua",
-    "client/onesync.lua",
     "dlc_xcustom/xsirens_one.awc",
 	"dlc_xcustom/xsirens_two.awc",
 	"dlc_xcustom/xsirens_three.awc",
@@ -30,24 +14,6 @@ client_scripts {
 	"dlc_xcustom/xsirens_seven.awc",
 	"data/xcustom_sounds.dat54.nametable",
 	"data/xcustom_sounds.dat54.rel"
-}
-
-server_scripts {
-    "server/server.lua",
-    "server/onesync.lua"
-}
-
-ui_page "html/index.html"
-
-files {
-    "els/*.json",
-    "html/index.html",
-    "html/sounds/SirenSwitch.ogg"
-}
-
-escrow_ignore {
-    "els/*.json",
-    "config.lua"
 }
 
 data_file "AUDIO_WAVEPACK" "dlc_xcustom"
