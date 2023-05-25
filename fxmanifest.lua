8b15e4287a51d9136ed2736f1bc5892dbed4bc28
the-revival-public <116266395+the-revival-public@users.noreply.github.com>
2023-05-25 13:45:40 +0100

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,20 +1,18 @@
 fx_version "bodacious"
 games { "gta5" }
 author 'Jay'
-author 'Made by Jay (SlumCity#0001)'
+author 'Made by Jay Discord:   ``#1234'
 lua54 'yes'
-
 client_scripts {
-    "dlc_xcustom/xsirens_one.awc",
-	"dlc_xcustom/xsirens_two.awc",
-	"dlc_xcustom/xsirens_three.awc",
-	"dlc_xcustom/xsirens_four.awc",
-	"dlc_xcustom/xsirens_five.awc",
-	"dlc_xcustom/xsirens_six.awc",
-	"dlc_xcustom/xsirens_seven.awc",
-	"data/xcustom_sounds.dat54.nametable",
-	"data/xcustom_sounds.dat54.rel"
+"dlc_xcustom/xsirens_one.awc",
+"dlc_xcustom/xsirens_two.awc",
+"dlc_xcustom/xsirens_three.awc",
+"dlc_xcustom/xsirens_four.awc",
+"dlc_xcustom/xsirens_five.awc",
+"dlc_xcustom/xsirens_six.awc",
+"dlc_xcustom/xsirens_seven.awc",
+"data/xcustom_sounds.dat54.nametable",
+"data/xcustom_sounds.dat54.rel"
 }
-
 data_file "AUDIO_WAVEPACK" "dlc_xcustom"
 data_file "AUDIO_SOUNDDATA" "data/xcustom_sounds.dat"
